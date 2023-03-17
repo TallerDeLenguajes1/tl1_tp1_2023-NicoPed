708bc30095fd9f5771b1d6bca62115ab4318086b
NicoPed <nikopedraza10@gmail.com>
2023-03-16 23:39:45 -0300

    Realizo el punto 3

--- tp1_1.c
+++ tp1_1.c
@@ -6,6 +6,14 @@ int main (){
 
     printf("Hola Mundo :)");
 
+    int num = 7, *pnum ;
+    pnum = &num;
+
+    printf("\nDirección de memoria almacenada por el puntero: %d", pnum);
+    printf("\nDirección de memoria de la variable: %d", &num);
+    printf("\nDirección de memoria del puntero: %d", &pnum);
+    printf("\nTamaño de memoria utilizado por esa variable usando la funcion: %d", sizeof(int));
+    printf("\nContenido del puntero: %d", *pnum);
     return 0;
 
 }
